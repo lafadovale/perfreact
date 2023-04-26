7e1a07d882881d0d547036d01b846eb4599048f3
Lafaiete do Vale <lafa.vale@gmail.com>
2023-04-25 23:01:07 -0300

    Application working

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,25 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
+import { FormEvent, useState } from "react";
+import { SearchResults } from "@/components/SearchResults";
 
 export default function Home() {
+  const [search, setSearch] = useState("");
+  const [results, setResults] = useState([]);
+
+  async function handleSearch(e: FormEvent) {
+    e.preventDefault();
+
+    if (!search.trim()) {
+      return;
+    }
+
+    const response = await fetch(`http://localhost:3333/products?q=${search}`);
+    const data = await response.json();
+
+    setResults(data);
+  }
+
   return (
     <>
       <Head>
@@ -11,7 +29,19 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <h1>Hello World</h1>
+        <div>
+          <h1>Search</h1>
+          <form onSubmit={handleSearch}>
+            <input
+              type="text"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button type="submit">Buscar</button>
+          </form>
+
+          <SearchResults results={results} />
+        </div>
       </main>
     </>
   );
